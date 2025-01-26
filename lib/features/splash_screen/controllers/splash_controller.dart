52e0f2d78ac80ef61636baada34f6af4db8ec49f
alphabetic100 <mh3214002@gmail.com>
2025-01-26 17:50:38 +0600

    Update SplashController to navigate based on authentication status

--- lib/features/splash_screen/controllers/splash_controller.dart
+++ lib/features/splash_screen/controllers/splash_controller.dart
@@ -1,3 +1,5 @@
+import 'package:courierapp/core/services/Auth_service.dart';
+import 'package:courierapp/features/landing/presentation/screens/landing_screen.dart';
 import 'package:courierapp/features/splash_screen/presentation/screens/onboarding_screen_1.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,12 +9,16 @@ class SplashController extends GetxController {
     Future.delayed(
       const Duration(milliseconds: 1500),
       () {
-        Get.offAll(
-          () => const OnboardingScreen1(),
-          transition: Transition.rightToLeftWithFade,
-          duration: Duration(milliseconds: 400),
-          curve: Curves.easeOut,
-        );
+        if (AuthService.hasToken()) {
+          Get.offAll(() => LandingScreen());
+        } else {
+          Get.offAll(
+            () => const OnboardingScreen1(),
+            transition: Transition.rightToLeftWithFade,
+            duration: Duration(milliseconds: 400),
+            curve: Curves.easeOut,
+          );
+        }
       },
     );
   }
